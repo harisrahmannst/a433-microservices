f6871c8968b642384aa693f86dfe6ea27b13d397
haris_nasution <rahmanharis90@gmail.com>
2022-11-29 22:57:11 +0700

    shell script

---
+++ build_push_images_karsajobs_ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# melakukan docker build
+docker build -t ghcr.io/harisrahmannst/karsajobs-ui:latest .
+
+# Login Github Package
+# menajalankan "export PASSWORD_DOCKER_HUB =YOUR_TOKEN"
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u harisrahmannst --password-stdin
+
+# melakukan push image ke github package
+docker push ghcr.io/harisrahmannst/karsajobs-ui:latest
